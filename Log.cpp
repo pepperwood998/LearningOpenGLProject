2d8cbec5df8c3f6da63d7b3cd71ebf8500a04679
zuratama <tuandt66742@gmail.com>
2019-01-15 22:54:33 +0700

    Adding Logging feature

---
+++ Log.cpp
@@ -0,0 +1,18 @@
+#include "Log.hpp"
+#include <iostream>
+#include <string>
+
+void unnamed::Log(const char *tag, const char *message)
+{
+    std::cout << tag << "\n>> " << message << std::endl;
+}
+
+void unnamed::SimpleLog(const char *message)
+{
+    std::cout << message << std::endl;
+}
+
+void unnamed::SimpleLog(float message)
+{
+    SimpleLog(std::to_string(message).c_str());
+}
---
+++ Log.hpp
@@ -0,0 +1,11 @@
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+
+namespace unnamed
+{
+    void Log(const char *tag, const char *message);
+    void SimpleLog(const char *message);
+    void SimpleLog(float message);
+};
+
+#endif // __LOG_HPP__
